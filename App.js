6a78846183227a9142f9323541118b6eca6bd784
shivgaurav <shivgaurav27@gmail.com>
2021-03-30 18:22:31 +0530

    center the header titles

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import "react-native-gesture-handler"
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
+import {StyleSheet, Text, View} from 'react-native';
 import Login from './components/Screens/Login';
 import Register from './components/Screens/Register';
-import { NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack"
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 const App = () => {
-  const Stack = createStackNavigator()
+  const Stack = createStackNavigator();
   return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Login" component={Login} />
-       <Stack.Screen name="Register" component={Register} />
-     </Stack.Navigator>
-   </NavigationContainer>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: 'Login',
+            headerTitleAlign: 'center',
+          }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={{
+            title: 'Register',
+            headerTitleAlign: 'center',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
-
