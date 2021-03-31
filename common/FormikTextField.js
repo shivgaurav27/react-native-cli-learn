185ee6ac674747e31581cf67c60f2cefaa5f4049
shivgaurav <shivgaurav27@gmail.com>
2021-03-31 17:09:11 +0530

    added

--- common/FormikTextField.js
+++ common/FormikTextField.js
@@ -1,55 +1,45 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Input } from 'react-native-elements';
-import { TextInput } from 'react-native-gesture-handler';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Input} from 'react-native-elements';
+import {fontSizes} from '../utils/Sizes';
+const FormikTextField = props => {
+  const {
+    field: {name, onBlur, onChange, value},
+    form: {errors, touched, setFieldTouched},
+    ...inputProps
+  } = props;
 
-const FormikTextField = (props) => {
+  const hasError = errors[name] && touched[name];
 
-    const {
-        field: { name, onBlur, onChange, value },
-        form: { errors, touched, setFieldTouched },
-        ...inputProps
-    } = props;
+  return (
+    <View style={styles.inputContainer}>
+      <Input
+        style={[hasError && styles.errorInput]}
+        value={value}
+        onChangeText={text => onChange(name)(text)}
+        onBlur={() => {
+          setFieldTouched(name);
+          onBlur(name);
+        }}
+        {...inputProps}
+      />
+      {hasError && <Text style={styles.errorText}>{errors[name]}</Text>}
+    </View>
+  );
+};
 
-    const hasError = errors[name] && touched[name];
-
-
-
-
-    return (
-        <View style={styles.inputContainer}>
-            <Input
-                style={[
-
-                    hasError && styles.errorInput
-                ]}
-                value={value}
-                onChangeText={(text) => onChange(name)(text)}
-                onBlur={() => {
-                    setFieldTouched(name)
-                    onBlur(name)
-                }}
-                {...inputProps}
-            />
-            {hasError && <Text style={styles.errorText}>{errors[name]}</Text>}
-        </View>
-    )
-}
-
-export default FormikTextField
+export default FormikTextField;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        width: 300
-    },
-    errorText: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft:"5%"
-        
-    },
-    errorInput: {
-        borderColor: 'red',
-        
-    }
-})
+  inputContainer: {
+    width: 300,
+  },
+  errorText: {
+    fontSize: fontSizes.md,
+    color: 'red',
+    marginLeft: '5%',
+  },
+  errorInput: {
+    borderColor: 'red',
+  },
+});
