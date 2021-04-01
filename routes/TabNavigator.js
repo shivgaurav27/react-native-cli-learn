854fb507fa3460bdaace4835d91fef5f6935e83f
shivgaurav <shivgaurav27@gmail.com>
2021-04-01 12:00:51 +0530

    make np project

--- routes/TabNavigator.js
+++ routes/TabNavigator.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+// import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
 import {
   MainStackNavigator,
   CategoriesStackNavigator,
@@ -13,12 +17,33 @@ const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+          if (route.name === 'Home') {
+            return(
+              <Ionicons name='ios-information-circle' size={size} color={color} />
+            )
+          }
+         
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+      
+      >
       <Tab.Screen name="Home" component={MainStackNavigator} />
       <Tab.Screen name="Categories" component={CategoriesStackNavigator} />
       <Tab.Screen name="Search" component={SearchStackNavigator} />
       <Tab.Screen name="My List" component={MyListStackNavigator} />
-      <Tab.Screen name="Basket" component={BasketStackNavigator} />
+      <Tab.Screen
+        name="Basket"
+        component={BasketStackNavigator}
+        options={{tabBarBadge: 3}}
+      />
     </Tab.Navigator>
   );
 };
