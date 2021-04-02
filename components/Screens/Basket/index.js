f01275189afde6db9e0baffbfb7aa60603426c04
shivgaurav <shivgaurav@codeblock.co.in>
2021-04-02 14:20:48 +0530

    added gitignore

--- components/Screens/Basket/index.js
+++ components/Screens/Basket/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import CustomListItem from '../../../common/CustomList';
 
-const Basket = () => {
+const DATA = [
+  {
+    id: '1',
+    title: 'Baby Apple Shimla',
+    qty: '1 kg - (Approx 7-8 pcs)',
+    Amount: 137,
+    url:
+      'https://images.unsplash.com/photo-1616679268603-0c13102c9b8d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE2OTMyOTk1&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
+  },
+  {
+    id: '2',
+    title: 'Orange',
+    Amount: 80,
+    qty: '2 kg - (Approx 7-8 pcs)',
+
+    url:
+      'https://images.unsplash.com/photo-1616679268603-0c13102c9b8d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE2OTMyOTk1&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
+  },
+];
+
+const Basket = ({navigation}) => {
   return (
-    <View>
-      <Text>Basket component</Text>
+    <View style={styles.center}>
+      <CustomListItem DATA={DATA} />
     </View>
   );
 };
 
 export default Basket;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+});
