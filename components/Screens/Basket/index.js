6ec98c31109510d6cce1ccebcfa690a0e1e68399
shivgaurav <shivgaurav@codeblock.co.in>
2021-04-03 17:09:04 +0530

    updated basket added

--- components/Screens/Basket/index.js
+++ components/Screens/Basket/index.js
@@ -3,32 +3,12 @@ import {StyleSheet, View} from 'react-native';
 import {connect} from 'react-redux';
 import CustomListItem from '../../../common/CustomList';
 
-const DATA = [
-  {
-    id: '1',
-    title: 'Baby Apple Shimla',
-    qty: '1 kg - (Approx 7-8 pcs)',
-    Amount: 137,
-    url:
-      'https://images.unsplash.com/photo-1616679268603-0c13102c9b8d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE2OTMyOTk1&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
-  },
-  {
-    id: '2',
-    title: 'Orange',
-    Amount: 80,
-    qty: '2 kg - (Approx 7-8 pcs)',
-
-    url:
-      'https://images.unsplash.com/photo-1616679268603-0c13102c9b8d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218fHx8fHx8fHwxNjE2OTMyOTk1&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300',
-  },
-];
-
 const Basket = props => {
   const {navigation, cartItems} = props;
 
   return (
     <View style={styles.center}>
-      <CustomListItem DATA={DATA} />
+      <CustomListItem DATA={cartItems} />
     </View>
   );
 };
