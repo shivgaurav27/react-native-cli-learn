799365942d4d41680df63ffdbe224e03112d4126
shivgaurav <shivgaurav@codeblock.co.in>
2021-04-03 18:05:35 +0530

    checkout at very bottom

--- components/Screens/Basket/index.js
+++ components/Screens/Basket/index.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgb(5, 93, 151)',
     position: 'absolute',
-    bottom: 20,
+    bottom: 0,
     width: '100%',
   },
   subContainer: {
